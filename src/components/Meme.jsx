4276f8d5e8ab537cec8977a8c36f1a7566b7ff36
Liz Towoju <liztowoju@gmail.com>
2022-12-15 01:47:42 +0100

    refactoring/change-css-names

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -4,8 +4,8 @@ export default function Meme(props) {
     return (
         <div className="meme">
             <img src={props.meme.url} className="img-fluid" alt="" />
-            <h2 className="meme--text top">{props.meme.topText}</h2>
-            <h2 className="meme--text bottom">{props.meme.bottomText}</h2>
+            <h2 className="meme-heading top">{props.meme.topText}</h2>
+            <h2 className="meme-heading bottom">{props.meme.bottomText}</h2>
         </div>
     );
 }
