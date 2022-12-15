f05c29c91757b133d02ad4db3d2770717d942c35
Liz Towoju <liztowoju@gmail.com>
2022-12-15 01:23:24 +0100

    feature/header-component-added

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+      <header className="navbar bg-light">
+        <div className="container">
+          <a className="navbar-brand fs-3" href="#">
+            Memely
+          </a>
+          <span>Build your own Meme</span>
+        </div>
+      </header>
+    )
+  }
